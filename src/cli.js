bfa2761f8d81212cc1c84a339d0d45143c8fc314
Rydkin Vitali <vrydkin@ibagroup.eu>
2025-10-26 14:22:27 +0300

    add system info

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,7 @@ import readline from 'node:readline';
 import os from 'node:os';
 import { up, cd, ls } from './navigate.js';
 import { cat, add, mkdir, rn, cp, mv, rm } from './files.js';
+import { osInfo } from './systemInfo.js';
 
 export async function startCli(username) {
     const rl = readline.createInterface({
@@ -32,42 +33,50 @@ export async function startCli(username) {
         const [command, ...args] = input.split(' ');
 
         try {
-            switch (command) {
-                case 'cat':
-                    await cat(args[0]);
-                    break;
-                case 'add':
-                    await add(args[0]);
-                    break;
-                case 'mkdir':
-                    await mkdir(args[0]);
-                    break;
-                case 'rn':
-                    await rn(args[0], args[1]);
-                    break;
-                case 'cp':
-                    await cp(args[0], args[1]);
-                    break;
-                case 'mv':
-                    await mv(args[0], args[1]);
-                    break;
-                case 'rm':
-                    await rm(args[0]);
-                    break;
-                case 'up':
-                    await up();
-                    break;
-                case 'cd':
-                    if (!args[0]) throw new Error('Invalid input');
-                    await cd(args.join(' '));
-                    break;
-                case 'ls':
-                    await ls();
-                    break;
-                case '':
-                    break;
-                default:
+            if (command === 'os') {
+                if (!args[0]) {
                     console.log('Invalid input');
+                } else {
+                    osInfo(args[0]);
+                }
+            } else {
+                switch (command) {
+                    case 'cat':
+                        await cat(args[0]);
+                        break;
+                    case 'add':
+                        await add(args[0]);
+                        break;
+                    case 'mkdir':
+                        await mkdir(args[0]);
+                        break;
+                    case 'rn':
+                        await rn(args[0], args[1]);
+                        break;
+                    case 'cp':
+                        await cp(args[0], args[1]);
+                        break;
+                    case 'mv':
+                        await mv(args[0], args[1]);
+                        break;
+                    case 'rm':
+                        await rm(args[0]);
+                        break;
+                    case 'up':
+                        await up();
+                        break;
+                    case 'cd':
+                        if (!args[0]) throw new Error('Invalid input');
+                        await cd(args.join(' '));
+                        break;
+                    case 'ls':
+                        await ls();
+                        break;
+                    case '':
+                        break;
+                    default:
+                        console.log('Invalid input');
+                }
             }
         } catch {
             console.error('Operation failed');
